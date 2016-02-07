fddefab087044450bc94a8a1b80d0ed651e02eaa
DEMI Demi <FOSPC@users.noreply.github.com>
2016-02-07 12:58:59 +0000

    07022016

--- module/Lmpe/src/Lmpe/Controller/LmpeController.php
+++ module/Lmpe/src/Lmpe/Controller/LmpeController.php
@@ -7,9 +7,15 @@ use Zend\View\Model\ViewModel;
 
 class LmpeController extends AbstractActionController {
 	public function indexAction() {
+        
+        $adapter=$this->getServiceLocator()->get('Zend/Db/Adapter/Adapter');
+
 		return new ViewModel ();
 	}
 	public function loginAction() {
+		return new ViewModel ();
+	}
+	public function signupAction() {
 		return new ViewModel ();
 	}
 }
